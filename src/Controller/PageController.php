6c08ba6c4cda92b6aa090fa57fd5e82d348096f1
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 19:12:10 -0600

    Se agrega formulario v1

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,13 +6,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
-    public function index(): Response
+    #[Route('/contact-v1', methods:['GET', 'POST' ])]
+    public function contactV1(): Response
     {
-        return $this->render('page/index.html.twig', [
-            'controller_name' => 'PageController',
+        $form = $this->createFormBuilder()
+            ->add('email', TextType::class, [
+                'label' => 'Correo electrónico',
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Comentario, sugerencia o mensaje',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enviar',
+            ])
+            ->setMethod('POST') //Default POST, puede ser GET o PUT
+            ->setAction('otra-url')
+            ->getForm();
+
+        return $this->render('page/contact-v1.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
