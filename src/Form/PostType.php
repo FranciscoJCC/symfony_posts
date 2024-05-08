f7fb7cb335a01a2a0b0017d980b94e137cd03637
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 21:40:27 -0600

    Se personaliza formulario posts y se habilita el csrf

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,13 +7,27 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class, [
+                'label' => 'Titulo de la publicación',
+                'help' => 'Piensa en el SEO ¿Cómo buscarías en Google?'
+            ])
+            ->add('body', TextareaType::class, [
+                'label' => 'Contenido',
+                'attr' => [ 'rows' => 9, 'class' => 'bg-light']
+            ])
+            ->add('Enviar', SubmitType::class, [
+                'attr' => ['class' => 'btn-primary']
+            ])
         ;
     }
 
@@ -21,6 +35,7 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
+            //'csrf_protection' => false, //Se puede deshabilitar el csrf para un formulario
         ]);
     }
 }
