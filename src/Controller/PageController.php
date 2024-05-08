ecf13597a9fd3d48e659ffea29666a5627d5707a
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 19:17:04 -0600

    Validamos si se envia el formulario

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -13,7 +14,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class PageController extends AbstractController
 {
     #[Route('/contact-v1', methods:['GET', 'POST' ])]
-    public function contactV1(): Response
+    public function contactV1(Request $request): Response
     {
         $form = $this->createFormBuilder()
             ->add('email', TextType::class, [
@@ -25,9 +26,15 @@ class PageController extends AbstractController
             ->add('save', SubmitType::class, [
                 'label' => 'Enviar',
             ])
-            ->setMethod('POST') //Default POST, puede ser GET o PUT
-            ->setAction('otra-url')
+            //->setMethod('POST') //Default POST, puede ser GET o PUT
+            //->setAction('otra-url')
             ->getForm();
+
+        $form->handleRequest($request);
+        if( $form->isSubmitted()){
+            //getData() tiene todos los valores que se han enviado
+            dd($form->getData(), $request);
+        }
 
         return $this->render('page/contact-v1.html.twig', [
             'form' => $form->createView(),
