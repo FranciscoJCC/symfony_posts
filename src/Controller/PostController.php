86f84893afc8b47c93b3256b8a5adb8717a6795f
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 22:59:41 -0600

    Actualizacion de un post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Form\PostType;
 
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/create', name: 'post_create')]
+    #[Route('/post/create', name: 'post_create', methods: ['GET', 'POST'])]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
@@ -24,11 +25,34 @@ class PostController extends AbstractController
             $entityManager->persist($form->getData());
             $entityManager->flush();
 
-            $this->addFlash('success', 'Prueba form #1 con éxito');
+            $this->addFlash('success', 'Publicación creada con éxito');
             return $this->redirectToRoute('post_create');
         }
 
         return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/post/{id}/update', name: 'post_update', methods: ['GET', 'POST'])]
+    public function update(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+
+        //Solo si se envia el formulario
+        $form->handleRequest($request);
+        if( $form->isSubmitted()){
+            //Guardamos en la bdd
+            //$entityManager->persist($form->getData()); //Linea opcional en el update
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Publicación editada con éxito');
+            return $this->redirectToRoute('post_update', [
+                'id' => $post->getId()
+            ]);
+        }
+
+        return $this->render('post/update.html.twig', [
             'form' => $form->createView(),
         ]);
     }
