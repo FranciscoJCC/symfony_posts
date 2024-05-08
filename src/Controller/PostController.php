63aa1b276f0ba0e7bda66eaa5641f88ec82f15f2
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 21:26:37 -0600

    Se crea vista, controlador y formulario para posts

---
+++ src/Controller/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PostType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/post/create', name: 'post_create')]
+    public function index(): Response
+    {
+        $form = $this->createForm(PostType::class);
+
+        return $this->render('post/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
