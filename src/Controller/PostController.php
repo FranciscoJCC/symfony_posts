7700a1e9ede7d24a9057b1acbeb0e6b3f2dc6d84
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 22:47:59 -0600

    Se guardan datos enviados del formulario de posts

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,16 +4,29 @@ namespace App\Controller;
 
 use App\Form\PostType;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/post/create', name: 'post_create')]
-    public function index(): Response
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class);
+
+        //Solo si se envia el formulario
+        $form->handleRequest($request);
+        if( $form->isSubmitted()){
+            //Guardamos en la bdd
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Prueba form #1 con éxito');
+            return $this->redirectToRoute('post_create');
+        }
 
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),
