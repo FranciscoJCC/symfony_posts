ac542bd629d03e470ddf41f01eb249a5e7763cfa
Francisco Cervantes <fjccandelario@gmail.com>
2024-05-07 21:08:34 -0600

    Se agrega templrate de navbar y alert

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -13,6 +13,13 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PageController extends AbstractController
 {
+
+    #[Route('/', name: 'index', methods:['GET'])]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig');
+    }
+
     #[Route('/contact-v1', name: 'contact-v1', methods:['GET', 'POST' ])]
     public function contactV1(Request $request): Response
     {
